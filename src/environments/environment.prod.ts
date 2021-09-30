58789d1f09f72850722598751095c0f3017e5ef3
FacundoFalcioni2410 <facundofalcioni2410@gmail.com>
2021-09-30 10:02:10 -0300

    parte 3 terminada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyALqrKBeIbXLMPAcugMK-EPEHklZiVgM60",
+    authDomain: "simulacroparciallaboiv.firebaseapp.com",
+    projectId: "simulacroparciallaboiv",
+    storageBucket: "simulacroparciallaboiv.appspot.com",
+    messagingSenderId: "974169694101",
+    appId: "1:974169694101:web:f49ae54523e9586b14209c"
+  }
 };
